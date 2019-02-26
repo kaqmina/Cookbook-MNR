f7e3e47549aab66c7bbef950493e82ca0d487b59
kaqmina <kannmina1023@gmail.com>
2019-02-26 13:54:49 +0800

    Boolean fielf for recipe deletion.
    'Delete' but only archiving.

--- cookbook/recipes/models.py
+++ cookbook/recipes/models.py
@@ -47,6 +47,7 @@ class Recipe(models.Model):
     cook_time = models.DurationField()
     read_time = models.DurationField()
     picture = models.ImageField(upload_to='recipe_img', null=True)
+    is_archived = models.BooleanField(default=False)
 
     def __str__(self):
         return '{}'.format(self.name)
