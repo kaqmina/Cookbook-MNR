299c0b4a44dd8534a852612ea5bbc83e53c24682
kaqmina <kannmina1023@gmail.com>
2019-03-02 20:45:31 +0800

    Updated home.html
    Summary:
    - added necessary components for home (w/o CSS)
    
    Details:
    - added html elements
    - added cards
    - added carousel
    - added base-2.html as base for logged in user.
    - normalized fonts.
    
    Not yet integrated (home):
    - django backend to template
    - urls
    - css formats for elements
    - footer
    - star ratings
    
    Next task:
    - template for recipe view.
    
    What we need:
    - design for categorical view.

--- cookbook/recipes/urls.py
+++ cookbook/recipes/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'recipes'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register', views.user_create, name='register'),
     path('create/', views.recipe_create, name='create_recipe'),
     path('home', views.home, name='home'),
-    path('register', views.user_create, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
 ]
